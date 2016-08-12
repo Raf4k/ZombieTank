91717171fa58b14eb301ddeeb410f439cdf662db
Raf4k <kampa.rafal@icloud.com>
2016-08-12 20:04:01 +0200

    base project

---
+++ ZombieTank/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  ZombieTank
+//
+
+//  Copyright (c) 2016 Rafal Kampa. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ ZombieTank/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  ZombieTank
+//
+
+//  Copyright (c) 2016 Rafal Kampa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
